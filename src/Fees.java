d66f6b54827bb1a5cf14a319dd8c893c7c86e287
Jared Montalbo <33338660+jaredmontalbo@users.noreply.github.com>
2020-02-29 02:24:23 -0500

    Create Fees.java
    - Contains static variables used to calculate tution fees

---
+++ src/Fees.java
@@ -0,0 +1,10 @@
+
+public class Fees {
+   public static final int IN_STATE_TUTION_FEE = 433;
+   public static final int OUT_OF_STATE_TUITION_FEE = 756;
+   public static final int INTERNAIONAL_TUITION_FEE = 945;
+   public static final int PART_TIME_FEE = 846;
+   public static final int FULL_TIME_FEE = 1441;
+   public static final int INTERNATIONAL_FEE = 350;
+
+}
