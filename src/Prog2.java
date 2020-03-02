7244c26c876b04f8a759183dd39c9a7c7d99c125
noahturbs <36610564+noahturbs@users.noreply.github.com>
2020-03-02 18:15:53 -0500

    final update. is format ok?

---
+++ src/Prog2.java
@@ -0,0 +1,12 @@
+/**
+  This class runs our tuition manager application where you can create and manage a
+  list consisting of the students, their info, and their tuition amount.
+ @author  Jared Montalbo, Noah Turbin
+ */
+public class Prog2
+{
+   public static void main(String [] args)
+   {
+      new TuitionManager().run();
+   } 
+}
